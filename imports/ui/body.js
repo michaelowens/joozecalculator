541f759900d819162299ed9d708167d58f39dce6
Djilano Smit <djilano@webartisans.nl>
2018-03-07 13:08:56 +0100

    nicotine units

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,16 +1,36 @@
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Recipes } from '../api/recipes.js';
 
 import './body.html';
 
 let formErrors = new ReactiveVar([]);
+let localData = new ReactiveDict({
+  nicotinemv: 1 // Default mg/ml
+});
+
+Template.registerHelper('instance', function () {
+  return Template.instance();
+});
+
+Template.recipeform.onCreated(function () {
+  this.localData = new ReactiveDict()
+  this.localData.set('basenicotinestr', 18)
+  this.localData.set('aimnicotinestr', 6)
+  this.localData.set('nicotinemv', 1); // Default mg/ml
+})
 
 Template.recipeform.helpers({
   checkError(field) {
     return formErrors.get().includes(field) && 'error'
+  },
+
+  nicotineUnits() {
+    console.log('get units', Template.instance().localData.get('nicotinemv'))
+    return Template.instance().localData.get('nicotinemv') === 2 ? '%' : 'mg/ml'
   }
 });
 
@@ -37,6 +57,7 @@ Template.recipeform.events({
     const target = event.target;
     const name = target.name.value;
     const public = target.public.checked;
+    const nicotinemv = target.nicotinemv.value;
     const instance = Template.instance();
 
     let errors = []
@@ -60,5 +81,34 @@ Template.recipeform.events({
 
     // Clear form
     target.name.value = '';
+  },
+
+  'input .basenicotinestr'(event) {
+    console.log(event.target.value);
+    Template.instance().localData.set('basenicotinestr', parseInt(event.target.value));
+  },
+
+  'input .aimnicotinestr'(event) {
+    console.log(event.target.value);
+    Template.instance().localData.set('aimnicotinestr', parseInt(event.target.value));
+  },
+
+  'change .nicotinemv'(event) {
+    let instance = Template.instance();
+    let val = parseInt(event.target.value);
+    console.log(event.target.value);
+    instance.localData.set('nicotinemv', val);
+
+    // values go either * 10 or / 10
+    let currentStr = instance.localData.get('basenicotinestr');
+    let aimcurrentStr = instance.localData.get('aimnicotinestr');
+
+    if (val === 2) {
+      instance.localData.set('basenicotinestr', currentStr / 10);
+      instance.localData.set('aimnicotinestr', aimcurrentStr / 10);
+    } else {
+      instance.localData.set('basenicotinestr', currentStr * 10);
+      instance.localData.set('aimnicotinestr', aimcurrentStr * 10);
+    }
   },
 });
